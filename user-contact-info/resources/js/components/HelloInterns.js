ecf562930df14dfdfb539e14c7040591f20050dc
sotesneal <nsotes@fullscale.io>
2022-05-16 11:20:50 +0800

    initial commit

---
+++ user-contact-info/resources/js/components/HelloInterns.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+ function HelloInterns() {
+    return (
+        <h1>Hello Interns!</h1>
+    );
+}
+
+
+ReactDOM.render(<HelloInterns />,document.getElementById("hello-react"));
