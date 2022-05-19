1f664bb5976be63c25b40f113f52e1c4742f556e
sotesneal <nsotes@fullscale.io>
2022-05-19 14:18:00 +0800

    fix some errors and added api endpoints

---
+++ user-contact-info/app/Http/Controllers/ContactController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\ContactResource;
+use App\Models\Contact;
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $contacts = Contact::paginate(10);
+        return ContactResource::collection($contacts);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $contact = new Contact();
+        $contact->firstname = $request->firstname;
+        $contact->lastname = $request->lastname;
+        $contact->email = $request->email;
+        $contact->phone = $request->phone;
+        $contact->deliveryAddress = $request->deliveryAddress;
+        $contact->billingAddress = $request->billingAddress;
+        if ($contact->save()) {
+            return new ContactResource($contact);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $contact = Contact::findOrFail($id);
+        return new ContactResource($contact);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $contact = Contact::findOrFail($id);
+        $contact->firstname = $request->firstname;
+        $contact->lastname = $request->lastname;
+        $contact->email = $request->email;
+        $contact->phone = $request->phone;
+        $contact->deliveryAddress = $request->deliveryAddress;
+        $contact->billingAddress = $request->billingAddress;
+        if ($contact->save()) {
+            return new ContactResource($contact);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $contact = Contact::find($id);
+        if ($contact->delete()) {
+            return new ContactResource($contact);
+        }
+    }
+}
--- user-contact-info/resources/js/Index.js
+++ user-contact-info/resources/js/Index.js
@@ -6,7 +6,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <>
-        <AppComponent />,
+        <AppComponent />
     </>
   </React.StrictMode>
 );
