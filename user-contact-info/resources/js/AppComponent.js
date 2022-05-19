6a7d7a2ddbb8a765fd8bbf90d7e786d7f0b9d1bc
mdegamo <mdegamo@fullscale.io>
2022-05-19 14:56:11 +0800

    Added data table

--- user-contact-info/resources/js/AppComponent.js
+++ user-contact-info/resources/js/AppComponent.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import HelloInterns from "./components/HelloInterns"
 import AddUser from "./components/AddUser";
+import DataTable from "./components/DataTable";
 
 export default function AppComponent(){
     return(
         <>
             <HelloInterns />
             <AddUser />
+            <DataTable />
         </>
     )
 }
