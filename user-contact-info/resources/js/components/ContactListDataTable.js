c36bba0a452134533317fac68f3f93c73d0b9f8d
emagnojr <emagno@fullscale.io>
2022-05-25 07:19:16 +0800

    Added Update and Delete User

--- user-contact-info/resources/js/components/ContactListDataTable.js
+++ user-contact-info/resources/js/components/ContactListDataTable.js
@@ -8,39 +8,11 @@ import React, { useState, useEffect } from "react";
 import "../../css/App.css";
 import MaterialTable from "material-table";
 
-import MUIDataTable from "mui-datatables";
-import IconButton from '@mui/material/IconButton';
-import Tooltip from '@mui/material/Tooltip';
-import EditIcon from '@mui/icons-material/Edit';
-import TableCell from '@mui/material/TableCell';
-
 const empList = [
-    {
-        fName: "Juan",
-        lName: "dela Cruz",
-        delAddress: "Fort Bonifacio Global City, Makati City, Metro Manila",
-        billAddress: "Fort Bonifacio Global City, Makati City, Metro Manila",
-    },
-    {
-        fName: "Isagani",
-        lName: "Vasquez",
-        delAddress:
-            "Casa Castelo Bldg., Maharlika Highway, Cabanatuan, Nueva Ecija",
-        billAddress:
-            "Casa Castelo Bldg., Maharlika Highway, Cabanatuan, Nueva Ecija",
-    },
-    {
-        fName: "Sam",
-        lName: "Santiago",
-        delAddress: "Session Road, Baguio, Benguet",
-        billAddress: "Prince Building, Makati City, Metro Manila",
-    },
-    {
-        fName: "Malaya",
-        lName: "Ignacio",
-        delAddress: "V. Rama Avenue, Guadalupe, Cebu City, Cebu",
-        billAddress: "V. Rama Avenue, Guadalupe, Cebu City, Cebu",
-    },
+    { fName: "Juan", lName: "dela Cruz", delAddress: "Fort Bonifacio Global City, Makati City, Metro Manila", billAddress: "Fort Bonifacio Global City, Makati City, Metro Manila"},
+    { fName: "Isagani", lName: "Vasquez", delAddress: "Casa Castelo Bldg., Maharlika Highway, Cabanatuan, Nueva Ecija", billAddress: "Casa Castelo Bldg., Maharlika Highway, Cabanatuan, Nueva Ecija"},
+    { fName: "Sam", lName: "Santiago", delAddress: "Session Road, Baguio, Benguet", billAddress: "Prince Building, Makati City, Metro Manila"},
+    { fName: "Malaya", lName: "Ignacio", delAddress: "V. Rama Avenue, Guadalupe, Cebu City, Cebu", billAddress: "V. Rama Avenue, Guadalupe, Cebu City, Cebu"},
     { fName: 'Max', lName: 'Reyes', delAddress: 'PVB Building, Justice Romualdez Street, Tacloban, Leyte', billAddress: 'PVB Building, Justice Romualdez Street, Tacloban, Leyte'},
     { fName: 'Shannon', lName: 'Diaz', delAddress: 'A. Pichon St., Davao City, Davao del Sur', billAddress: 'A. Pichon St., Davao City, Davao del Sur'},
     { fName: 'Jamie', lName: 'Castro', delAddress: '2/F JR Photo Lab Bldg., Quezon Avenue, Cotabato City, Maguindanao', billAddress: '2/F JR Photo Lab Bldg., Quezon Avenue, Cotabato City, Maguindanao'},
@@ -53,7 +25,7 @@ const empList = [
 
 export default function ContactListDataTable() {
 
-    const [data, setData] = useState(empList);
+    const [tableData, setTableData] = useState(empList);
     const columns = [
         { title: "First Name", field: "fName" },
         { title: "Last Name", field: "lName" },
@@ -66,20 +38,28 @@ export default function ContactListDataTable() {
             <h1 align="center">Contact Information</h1>
             <MaterialTable
                 title="Contact Information"
-                data={data}
+                data={tableData}
                 columns={columns}
                 editable={{
                     onRowAdd: (newRow) =>
                         new Promise((resolve, reject) => {
-                            setData([...data, newRow]);
+                            setTableData([...tableData, newRow]);
                             setTimeout(() => resolve(), 500);
                         }),
                     onRowUpdate: (newRow, oldRow) =>
                         new Promise((resolve, reject) => {
-                            const updatedData = [...data];
-                            updatedData[oldRow.data.id] = newRow;
-                            setData = updatedData;
+                            console.log(newRow, oldRow);
+                            const updatedData = [...tableData];
+                            updatedData[oldRow.tableData.id] = newRow;
+                            setTableData(updatedData);
                             setTimeout(() => resolve(), 500);
+                        }),
+                    onRowDelete: (selectedRow) =>
+                        new Promise((resolve, reject) => {
+                            const updatedData = [...tableData];
+                            updatedData.splice(selectedRow.tableData.id, 1);
+                            setTableData(updatedData);
+                            setTimeout(() => resolve(), 1000);
                         }),
                 }}
                 options={{
@@ -101,30 +81,4 @@ export default function ContactListDataTable() {
             />
         </div>
     );
-
-//   // Adding edit icon
-//   for (const arr of data) {
-//     arr.unshift(
-//       <div align="center">
-//         <Tooltip title="Edit contact information">
-//           <IconButton>
-//             <EditIcon/>
-//           </IconButton>
-//         </Tooltip>
-//       </div>
-//     )
-//   }
-
-//   const options = {
-//     filterType: 'checkbox',
-//   };
-
-//   return (
-//     <MUIDataTable
-//       title={"Contact List"}
-//       data={data}
-//       columns={columns}
-//       options={options}
-//     />
-//   );
 }
