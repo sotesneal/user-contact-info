73ac73894681bf485df0dfe43872468832d5173c
emagnojr <emagno@fullscale.io>
2022-05-24 13:53:00 +0800

    Switch from data tables to material table

--- user-contact-info/resources/js/AppComponent.js
+++ user-contact-info/resources/js/AppComponent.js
@@ -7,8 +7,8 @@ import Search from "./components/SearchContact";
 export default function AppComponent(){
     return(
         <div className="container">
-            <h1>Contact Information</h1>
-            <AddUser />
+            {/* <h1></h1>
+            <AddUser /> */}
             <ContactListDataTable />
         </div>
     )
